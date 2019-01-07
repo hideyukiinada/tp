8729a2c21097121ff4b964835d2392cec5d290d6
m <m@ms-Mac-mini.local>
2019-01-07 11:25:07 -0800

    load_text_data working.  Verified with a test script

---
+++ project/load_text_data.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+"""
+Load text data
+
+__author__ = "Hide Inada"
+__copyright__ = "Copyright 2018, Hide Inada"
+__license__ = "The MIT License"
+__email__ = "hideyuki@gmail.com"
+"""
+
+import os
+import logging
+
+import tensorflow as tf
+import numpy as np
+import keras
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import LSTM
+import sklearn.datasets
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))  # Change the 2nd arg to INFO to suppress debug logging
+
+
+def load_text_from_files(root_dir, test_dataset_ratio=0.2):
+    """
+    Load text from files under label directories.
+
+    Parameters
+    ----------
+    root_dir: pathlib.Path
+        Parent directory of labeled directories, each of which contains text files
+    test_dataset_ratio: float
+        Ratio of test dataset to split the data into training dataset and test dataset
+
+    Returns
+    -------
+    (x_train, y_train), (x_test, y_test): tuples of list
+        x_train and x_test contains the list of text in string.
+        y_train and y_test contains the list of labels in string.
+
+    Raises
+    ------
+    ValueError
+        If root_dir does not exist.
+    """
+    root_dir_path = root_dir
+    if root_dir_path.exists() is False:
+        log.fatal("% does not exist." % (root_dir_path))
+        raise ValueError("% does not exist." % (root_dir_path))
+
+    label_dirs = [x for x in root_dir_path.iterdir() if x.is_dir()]
+
+    all_training_samples = list()
+    all_test_samples = list()
+
+    for label_dir in label_dirs:
+
+        label = label_dir.name
+        log.info("Scanning %s" % (label))
+
+        samples_for_label = list()
+        # Process files
+        for input_file in label_dir.glob("*.txt"):
+            with open(input_file, "r") as f:
+                text = f.read()  # Change this if you are reading a very large text file.
+                sample = (text, label)
+                samples_for_label.append(sample)
+
+        num_samples_for_label = len(samples_for_label)
+        if len == num_samples_for_label:
+            continue
+
+        if num_samples_for_label == 1 and test_dataset_ratio > 0:
+            log.info("Label %s contain only 1 sample and cannot be split to training and test dataset. Skipping.")
+            continue
+
+        if test_dataset_ratio > 0:
+            num_test_samples = max(int(num_samples_for_label * test_dataset_ratio), 1)
+        else:
+            num_test_samples = 0
+
+        num_training_samples = num_samples_for_label - num_test_samples
+        training_samples = samples_for_label[:num_training_samples]
+        test_samples = samples_for_label[num_training_samples:]
+
+        all_training_samples += training_samples
+        all_test_samples += test_samples
+
+    # Unzip the list
+    (x_train, y_train) = tuple(zip(*all_training_samples))
+
+    if len(all_test_samples) > 0:
+        (x_test, y_test) = tuple(zip(*all_test_samples))
+    else:
+        x_test = list()
+        y_test = list()
+
+    return (x_train, y_train), (x_test, y_test)
---
+++ test/test_load_text_data.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+"""
+Unit test use case of a method that is used in tp.
+
+__author__ = "Hide Inada"
+__copyright__ = "Copyright 2018, Hide Inada"
+__license__ = "The MIT License"
+__email__ = "hideyuki@gmail.com"
+"""
+import unittest
+import os
+import logging
+from pathlib import Path
+
+import numpy as np
+from project.load_text_data import load_text_from_files
+
+TEST_DATA_DIR = Path("/tmp/tp/test")
+
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    level=os.environ.get("LOGLEVEL", "INFO"))  # Change the 2nd arg to INFO to suppress debug logging
+
+
+def create_test_files():
+    """
+    Create test files under the test data directory.
+    """
+
+    test_sample_dir = TEST_DATA_DIR
+
+    if test_sample_dir.exists() is False:
+        test_sample_dir.mkdir(parents=True, exist_ok=True)
+
+    # Create directories
+    dirs = ["apple",
+            "banana",
+            "cherry"]
+    for dir in dirs:
+        dir = test_sample_dir / Path(dir)
+        if dir.exists() is False:
+            dir.mkdir(parents=True, exist_ok=True)
+            log.info("%s created" % (dir))
+        else:
+            log.info("%s found. Skip creation" % (dir))
+
+    # Create files
+    files = ["apple/granny_smith_apple.txt",
+             "apple/fuji_apple.txt",
+             "apple/golden_delicious.txt",
+             "apple/gala.txt",
+             "apple/honeycrisp.txt",
+             "banana/banana.txt",  # Missing test data
+             "cherry/bing_cherry.txt",  # 1 test data
+             "cherry/rainier_cherry.txt"]
+    text_body = ["Granny Smith apples are green.",
+                 "Fuji apples are red.",
+                 "I love Golden Delicious apples.",
+                 "Gala apples are my favorite.",
+                 "I want to try an apple pie made out of Honeycrisp apples",
+                 "Bananas are good for breakfast.",
+                 "Bing cherries are popular.",
+                 "Rainer cherries are in season right now."]
+
+    for i, f in enumerate(files):
+        text_path = test_sample_dir / Path(f)
+        if text_path.exists() is False:
+            with open(text_path, "w") as fh:
+                fh.write(text_body[i])
+            log.info("%s written" % (text_path))
+
+
+class TestLoadTextData(unittest.TestCase):
+
+    def test_load_text(self):
+        """
+        Test loading text
+        """
+        create_test_files()
+
+        (x_train, y_train), (x_test, y_test) = load_text_from_files(TEST_DATA_DIR, test_dataset_ratio=0.2)
+
+        # x_train len
+        expected = 5  # 4 apples and 1 cherry
+        actual = len(x_train)
+
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        # x_test len
+        expected = 2  # 1 apples and 1 cherry
+        actual = len(x_test)
+
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+    def test_load_text_no_test(self):
+        """
+        Test loading text
+        """
+        create_test_files()
+
+        (x_train, y_train), (x_test, y_test) = load_text_from_files(TEST_DATA_DIR, test_dataset_ratio=0)
+
+        # x_train len
+        expected = 8  # 5 apples, 1 banana and 2 cherries
+        actual = len(x_train)
+
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        # x_train len
+        expected = 0
+        actual = len(x_test)
+
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+
+def main():
+    """Invoke test function"""
+
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()

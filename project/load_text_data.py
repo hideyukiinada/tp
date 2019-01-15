3691bdec981422d2e324f6dbc035cc536ecafb5f
m <m@ms-Mac-mini.local>
2019-01-14 18:19:05 -0800

    Initial check in of reuters 50 classification

--- project/load_text_data.py
+++ project/load_text_data.py
@@ -64,8 +64,6 @@ def load_text_from_files(root_dir, test_dataset_ratio=0.2, errors=None):
                 samples_for_label.append(sample)
 
         num_samples_for_label = len(samples_for_label)
-        if len == num_samples_for_label:
-            continue
 
         if num_samples_for_label == 1 and test_dataset_ratio > 0:
             log.info("Label %s contain only 1 sample and cannot be split to training and test dataset. Skipping.")
@@ -149,8 +147,6 @@ def load_text_and_label_id_from_files(root_dir, test_dataset_ratio=0.2, errors=N
                 samples_for_label.append(sample)
 
         num_samples_for_label = len(samples_for_label)
-        if len == num_samples_for_label:
-            continue
 
         if num_samples_for_label == 1 and test_dataset_ratio > 0:
             log.info("Label %s contain only 1 sample and cannot be split to training and test dataset. Skipping.")
@@ -176,6 +172,98 @@ def load_text_and_label_id_from_files(root_dir, test_dataset_ratio=0.2, errors=N
         all_training_samples += training_samples
         all_test_samples += test_samples
 
+
+    id_to_label = {id: label for label, id in label_to_id.items()}
+
+    # Unzip the list
+    (x_train, y_train) = tuple(zip(*all_training_samples))
+
+    if len(all_test_samples) > 0:
+        (x_test, y_test) = tuple(zip(*all_test_samples))
+    else:
+        x_test = list()
+        y_test = list()
+
+    y_train_np = np.array(y_train)
+    y_test_np = np.array(y_test)
+
+    return (x_train, y_train_np), (x_test, y_test_np), (label_to_id, id_to_label)
+
+def load_text_and_label_id_from_training_and_test_dirs(root_training_dir, root_test_dir, errors=None):
+    """
+    Load text from files under label directories.
+
+    Parameters
+    ----------
+    root_training_dir: pathlib.Path
+        Parent directory of labeled directories containing training dataset, each of which contains text files
+    root_test_dir: pathlib.Path
+        Parent directory of labeled directories containing test dataset, each of which contains text files
+    errors: str
+        Set to 'ignore' if you want to ignore a byte that cannot be decoded.
+
+    Returns
+    -------
+    (x_train, y_train): tuple of list
+        x_train contains the list of text in string. y_train contains the ndarray of label IDs in int.
+    (x_test, y_test): tuple of list
+        x_test contains the list of text in string. y_test contains the ndarray of label IDs in int.
+    (label_to_id, id_to_label): tuple of dictionary
+        label_to_id contains label to ID mapping.  id_to_label contains ID to label mapping.
+
+    Raises
+    ------
+    ValueError
+        If root_dir does not exist.
+    """
+
+    label_to_id = dict()
+    current_id = 0
+
+    all_training_samples = list()
+    all_test_samples = list()
+
+    label_dirs_training = None
+    for i, root_dir_path in enumerate([root_training_dir, root_test_dir]):
+
+        if root_dir_path.exists() is False:
+            log.fatal("% does not exist." % (root_dir_path))
+            raise ValueError("% does not exist." % (root_dir_path))
+
+        label_dirs = [x for x in root_dir_path.iterdir() if x.is_dir()]
+        if i == 0:
+            label_dirs_training = label_dirs
+        else:
+            if {p.name for p in label_dirs} != {p.name for p in label_dirs_training}:
+                raise ValueError("Training dataset has different label(s) from test dataset.")
+
+        for label_dir in label_dirs:
+
+            label = label_dir.name
+            log.info("Scanning %s" % (label))
+
+            samples_for_label = list()
+
+            # Process files for the label
+            for input_file in label_dir.glob("*"):
+                with open(input_file, "r", errors=errors) as f:
+                    text = f.read()  # Change this if you are reading a very large text file.
+                    sample = (text, label)
+                    samples_for_label.append(sample)
+
+            if i == 0: # if training
+                if label not in label_to_id:
+                    new_label_id = current_id
+                    label_to_id[label] = new_label_id
+                    current_id += 1
+
+            # Change label string to ID in dataset
+            samples = [(s[0], label_to_id[s[1]]) for s in samples_for_label]
+
+            if i == 0:
+                all_training_samples += samples
+            else:
+                all_test_samples += samples
 
     id_to_label = {id: label for label, id in label_to_id.items()}
 
--- test/test_load_text_data.py
+++ test/test_load_text_data.py
@@ -15,9 +15,13 @@ from pathlib import Path
 import numpy as np
 from project.load_text_data import load_text_from_files
 from project.load_text_data import load_text_and_label_id_from_files
+from project.load_text_data import load_text_and_label_id_from_training_and_test_dirs
+
 from project.text_to_id import map_label_to_id
 
 TEST_DATA_DIR = Path("/tmp/tp/test")
+TEST_DATA_DIR_SEPARATE_TRAINING = Path("/tmp/tp/separate/training_set")
+TEST_DATA_DIR_SEPARATE_TEST = Path("/tmp/tp/separate/test_set")
 
 log = logging.getLogger(__name__)
 logging.basicConfig(
@@ -70,6 +74,85 @@ def create_test_files():
             with open(text_path, "w") as fh:
                 fh.write(text_body[i])
             log.info("%s written" % (text_path))
+
+def create_test_files_training_test():
+    """
+    Create test files under separate training and test dataset directory.
+    """
+
+    training_dir = TEST_DATA_DIR_SEPARATE_TRAINING
+    test_dir = TEST_DATA_DIR_SEPARATE_TEST
+
+    if training_dir.exists() is False:
+        training_dir.mkdir(parents=True, exist_ok=True)
+
+    if test_dir.exists() is False:
+        test_dir.mkdir(parents=True, exist_ok=True)
+
+    # Training dataset
+    # Create directories
+    dirs = ["apple",
+            "banana",
+            "cherry"]
+    for dir in dirs:
+        dir = training_dir / Path(dir)
+        if dir.exists() is False:
+            dir.mkdir(parents=True, exist_ok=True)
+            log.info("%s created" % (dir))
+        else:
+            log.info("%s found. Skip creation" % (dir))
+
+    # Create files
+    files = ["apple/granny_smith_apple.txt",
+             "apple/fuji_apple.txt",
+             "apple/golden_delicious.txt",
+             "apple/gala.txt",
+             "apple/honeycrisp.txt",
+             "banana/banana.txt",  # Missing test data
+             "cherry/bing_cherry.txt",  # 1 test data
+             "cherry/rainier_cherry.txt"]
+    text_body = ["Granny Smith apples are green.",
+                 "Fuji apples are red.",
+                 "I love Golden Delicious apples.",
+                 "Gala apples are my favorite.",
+                 "I want to try an apple pie made out of Honeycrisp apples",
+                 "Bananas are good for breakfast.",
+                 "Bing cherries are popular.",
+                 "Rainer cherries are in season right now."]
+
+    for i, f in enumerate(files):
+        text_path = training_dir / Path(f)
+        if text_path.exists() is False:
+            with open(text_path, "w") as fh:
+                fh.write(text_body[i])
+            log.info("%s written" % (text_path))
+
+
+    # Test dataset
+    # Create directories
+    for dir in dirs:
+        dir = test_dir / Path(dir)
+        if dir.exists() is False:
+            dir.mkdir(parents=True, exist_ok=True)
+            log.info("%s created" % (dir))
+        else:
+            log.info("%s found. Skip creation" % (dir))
+
+    # Create files
+    files = ["apple/granny_smith_apple.txt",
+             "banana/banana.txt",  # Missing test data
+             "cherry/rainier_cherry.txt"]
+    text_body = ["Granny Smith apples good for apple pie.  They are a little sour though.",
+                 "Bananas are very nutricious.",
+                 "Rainer cherries are also good if you dry them."]
+
+    for i, f in enumerate(files):
+        text_path = test_dir / Path(f)
+        if text_path.exists() is False:
+            with open(text_path, "w") as fh:
+                fh.write(text_body[i])
+            log.info("%s written" % (text_path))
+
 
 
 class TestLoadTextData(unittest.TestCase):
@@ -181,6 +264,61 @@ class TestLoadTextData(unittest.TestCase):
         result = actual == expected
         self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
 
+    def test_load_text_and_label_id_from_separate_dirs(self):
+        """
+        Test loading text
+        """
+        create_test_files_training_test()
+
+        (x_train, y_train), (x_test, y_test), (label_to_id, id_to_label) = \
+            load_text_and_label_id_from_training_and_test_dirs(
+                TEST_DATA_DIR_SEPARATE_TRAINING,
+                TEST_DATA_DIR_SEPARATE_TEST)
+
+        # x_train len
+        expected = 8
+        actual = len(x_train)
+
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        # x_test len
+        expected = 3
+        actual = len(x_test)
+
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        # y_test len
+        expected = {0, 1, 2}
+        actual = set(y_test)
+
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        labels = label_to_id.keys()
+        actual = set(labels)
+        expected = {"apple", "banana", "cherry"}
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        ids = label_to_id.values()
+        actual = set(ids)
+        expected = {0, 1, 2}
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        labels = id_to_label.values()
+        actual = set(labels)
+        expected = {"apple", "banana", "cherry"}
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
+
+        ids = id_to_label.keys()
+        actual = set(ids)
+        expected = {0, 1, 2}
+        result = actual == expected
+        self.assertTrue(result, "actual does not match expected. \nActual:\n%s, \nExpected:\n%s" % (actual, expected))
 
 def main():
     """Invoke test function"""
